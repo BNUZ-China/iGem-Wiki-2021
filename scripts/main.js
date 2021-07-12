edd6f5c062e3c5a4935be7fd428e1286ab723cd0
ZhiXingHeYi-0712 <Lihao.0712@outlook.com>
2021-07-12 20:25:44 +0800

    finish front page animate

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,24 @@
-var app = new Vue({
-    el: '#bnuz-app',
-    data: {
-        message: 'hello!'
-    }
+// var zoom_animate = function () {
+//     con = $('.zoom_container')[0];
+//     con.animate({
+//         transform: 'scale(2.3)'
+//     }, {delay: 4000, duration: 3000, fill: "forwards", easing: 'ease'});
+//     con.animate({
+//         display: 'none'
+//     }, {duration: 3000, fill: "forwards", easing: 'ease'})
+// }
+//
+// $('.mask_layer_left')[0].animate({
+//     transform: 'rotateY(80deg)'
+// }, {duration: 3000, fill: "forwards", easing: 'ease'})
+//
+// $('.mask_layer_right')[0].animate({
+//     transform: 'rotateY(-80deg)'
+// }, {duration: 3000, fill: "forwards", easing: 'ease'})
+//
+// zoom_animate()
+
+zoom_container = $('.zoom_container')[0]
+zoom_container.addEventListener('animationend', function () {
+    zoom_container.hidden = true
 })
