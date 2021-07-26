44a63797d3751873e125007055d582a9dae445f2
ZhiXingHeYi-0712 <Lihao.0712@outlook.com>
2021-07-26 21:52:01 +0800

    finish template navigation and content navigation's moving.

--- scripts/template_init.js
+++ scripts/template_init.js
@@ -7,16 +7,19 @@ var numToPx = function (number) {
 }
 
 $(window).scroll(function() {
-    //为了保证兼容性，这里取两个值，哪个有值取哪一个
-    //scrollTop就是触发滚轮事件时滚轮的高度
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    /*
+    使用浮动样式的条件；正文文本框距顶距离小于屏幕高的三分之一
+     */
     var contentNav = document.getElementById('bnuz-content-nav-container');
+    var contentText = document.getElementById('bnuz-text-container');
+    var windowHeight = window.screen.height;
+    var trigger_value = windowHeight / 3;   // 临界高度
     var boundingRect = contentNav.getBoundingClientRect();
     var width = boundingRect.width;
-    var top = boundingRect.y;
+    var top = trigger_value;
     var left = boundingRect.x;
     // 取400为临界值
-    if (scrollTop > 400) {
+    if (contentText.getBoundingClientRect().y < trigger_value) {
         // 滚动到下方的样式
         contentNav.className = 'bnuz-content-nav-float';
         contentNav.style.width = numToPx(width);
