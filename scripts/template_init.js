78a93ca9858da91eed5c4a2de6d7b7056550b49a
ZhiXingHeYi-0712 <Lihao.0712@outlook.com>
2021-07-27 00:05:53 +0800

    完成滚动条草稿

--- scripts/template_init.js
+++ scripts/template_init.js
@@ -1,12 +1,43 @@
+"use strict";
+
 window.onload = function () {
     $('.dropdown-toggle').dropdown();
 };
 
 var numToPx = function (number) {
     return number.toString()+'px';
+};
+
+var anchors = document.getElementsByClassName('bnuz-content-anchor');
+var navItems = document.getElementsByClassName('bnuz-content-nav-item');
+var anchors_distance = [];
+for (var anchor of anchors) {
+    anchors_distance.push(anchor.getBoundingClientRect().top);
 }
 
-$(window).scroll(function() {
+var updateNav = function () {
+    var scroll_location = document.documentElement.scrollTop;
+    var k = undefined; // 表示第几个应该高亮
+    var k_defined = false;
+    for (var i = 0; i < anchors.length; i++) {
+        if (scroll_location > anchors_distance[i]) {
+            k = i;
+            k_defined = true;
+        }
+    }
+
+    if (!k_defined) return;
+
+    var active_elements = document.getElementsByClassName('bnuz-content-nav-item-active');
+    for (var active_element of active_elements) {
+        active_element.classList.remove('bnuz-content-nav-item-active');
+    }
+
+    var active_navItem = navItems[k];
+    active_navItem.classList.add('bnuz-content-nav-item-active');
+};
+
+var moveNav = function() {
     /*
     使用浮动样式的条件；正文文本框距顶距离小于屏幕高的三分之一
      */
@@ -29,4 +60,10 @@ $(window).scroll(function() {
         // 返回的样式
         contentNav.className = 'col-4';
     }
+};
+
+window.addEventListener('scroll', function () {
+    moveNav();
+    updateNav();
 })
+
